a99ad63d82e8b5e184f0c54e0991109659914ad5
thienv29 <thienv29@gmail.com>
2025-10-04 14:10:38 +0700

    save

--- src/core/providers/theme-provider.tsx
+++ src/core/providers/theme-provider.tsx
@@ -2,10 +2,15 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { SessionProvider } from "next-auth/react"
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <SessionProvider>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </SessionProvider>
+  )
 }
