477b561716bbad928e8242109e6deb5f84b7c144
thienv29 <thienv29@gmail.com>
2025-10-07 15:15:40 +0700

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,7 +17,7 @@ export default function DashboardLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex justify-between px-4 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="flex justify-between px-4 h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 ">
             <SidebarTrigger className="-ml-1" />
             <Separator
@@ -30,7 +30,7 @@ export default function DashboardLayout({
             <ThemeSwitcher className="scale-110" />
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0 mt-4">
           {children}
         </div>
       </SidebarInset>
